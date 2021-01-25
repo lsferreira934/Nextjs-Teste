71c8aabaf8507f212504f0a7768efd8f4d440a60
leandro <leandrosf934@gmail.com>
2021-01-25 10:30:20 -0300

    atualização

--- pages/api/cep.js
+++ pages/api/cep.js
@@ -6,6 +6,8 @@ async function buscaCep(req, res) {
   const checkCep = await fetch(`https://viacep.com.br/ws/${CEP}/json/`);
   const data = await checkCep.json();
 
+  res.setHeader('Cache-Control', 's-maxage=10, stale-while-revalidate');
+
   res.json({
     CEP: data,
     DATA: dynamicDate,
